c5db30de69eddfdb61fc2790db22829dc2d99593
Gurish Sharma <gurish@Gurishs-MacBook-Pro.local>
2024-03-20 22:05:31 -0400

    changes website header

--- src/components/RevealedNumbers.js
+++ src/components/RevealedNumbers.js
@@ -6,9 +6,11 @@ const RevealedNumbers = ({ revealedNumbers, onRemoveNumber }) => {
       <h3>Revealed Numbers:</h3>
       <ul>
         {revealedNumbers.map((number, index) => (
-          <li key={index}>
-            {number}
-            <button onClick={() => onRemoveNumber(index)} className="p-2" >X</button>
+          <li key={index} className="flex justify-between items-center m-2">
+            <span>{number}</span>
+            <button onClick={() => onRemoveNumber(index)} className="ml-4 bg-red-500 text-white px-2 rounded">
+              x
+            </button>
           </li>
         ))}
       </ul>
